645f9621f5295ed70085296a445f96c03a477c93
Pufke <pualic.nemanja@gmail.com>
2021-01-25 03:45:16 +0100

    Initial frontend project with Vue material

--- frontend-vue-js/src/App.vue
+++ frontend-vue-js/src/App.vue
@@ -1,11 +1,36 @@
 <template>
-  <div id="material-kit">
-    <div :class="{ 'nav-open': NavbarStore.showNavbar }">
-      <router-view name="header" />
-      <div>
-        <router-view />
-      </div>
-      <router-view name="footer" />
-    </div>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Vue from 'vue'
+import { MdButton, MdContent, MdTabs } from 'vue-material/dist/components'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
+
+Vue.use(MdButton)
+Vue.use(MdContent)
+Vue.use(MdTabs)
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
