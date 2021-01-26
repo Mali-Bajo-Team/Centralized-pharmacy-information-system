f10f2b628ba930f2754376b5be713b4a758450ae
Pufke <pualic.nemanja@gmail.com>
2021-01-26 03:56:48 +0100

    feat: Cross-origin resource sharing for GET API

--- backend-spring-boot/src/main/java/com/pharmacy/cpis/controller/UserController.java
+++ backend-spring-boot/src/main/java/com/pharmacy/cpis/controller/UserController.java
@@ -16,6 +16,7 @@ public class UserController {
     @Autowired
     private UserService userAccService;
 
+    @CrossOrigin
     @GetMapping
     public ResponseEntity<List<UserAccDTO>> getUsersAccs() {
 
