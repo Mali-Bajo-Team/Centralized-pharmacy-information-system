b228c88c3972d4f7e45b8e3929706b1e618117e3
Pufke <pualic.nemanja@gmail.com>
2021-01-27 01:44:19 +0100

    refact: reafactoring App.vue

--- frontend-vue-js/src/App.vue
+++ frontend-vue-js/src/App.vue
@@ -1,32 +1,26 @@
 <template>
   <div id="app">
       <div>
-    <md-tabs md-sync-route>
-      <md-tab id="tab-home" md-label="Home" to="/" exact>
-        Home Tab
-      </md-tab>
+        <md-tabs md-sync-route>
+            <md-tab id="tab-home" md-label="Home" to="/login" exact>
+            </md-tab>
 
-      <md-tab id="tab-pages" md-label="Pages" to="/allUsers">
-        Pages tab
-        <p>Unde provident nemo reiciendis officia, possimus repellendus. Facere dignissimos dicta quis rem. Aliquam aspernatur dolor atque nisi id deserunt laudantium quam repellat.</p>
-      </md-tab>
+            <md-tab id="tab-pages" md-label="Pages" to="/allUsers">
+              Pages tab
+            </md-tab>
 
-      <md-tab id="tab-posts" md-label="Posts" to="/components/tabs/posts">
-        Posts tab
-        <p>Qui, voluptas repellat impedit ducimus earum at ad architecto consectetur perferendis aspernatur iste amet ex tempora animi, illum tenetur quae assumenda iusto.</p>
-      </md-tab>
+            <md-tab id="tab-posts" md-label="Posts" to="/components/tabs/posts">
+              Posts tab
+              <p>Qui, voluptas repellat impedit ducimus earum at ad architecto consectetur perferendis aspernatur iste amet ex tempora animi, illum tenetur quae assumenda iusto.</p>
+            </md-tab>
 
-      <md-tab id="tab-favorites" md-label="Favorites" to="/components/tabs/favorites">
-        Favorites tab
-        <p>Maiores, dolorum. Beatae, optio tempore fuga odit aperiam velit, consequuntur magni inventore sapiente alias sequi odio qui harum dolorem sunt quasi corporis.</p>
-      </md-tab>
-
-       <md-tab id="tab-login" md-label="Login" to="/login">
-       
-      </md-tab>
-    </md-tabs>
-  </div>
-      <div>
+            <md-tab id="tab-favorites" md-label="Favorites" to="/components/tabs/favorites">
+              Favorites tab
+              <p>Maiores, dolorum. Beatae, optio tempore fuga odit aperiam velit, consequuntur magni inventore sapiente alias sequi odio qui harum dolorem sunt quasi corporis.</p>
+            </md-tab>
+       </md-tabs>
+      </div>
+    <div>
     <router-view></router-view>
   </div>
     
@@ -37,15 +31,11 @@
 
 import VueMaterial from 'vue-material'
 import Vue from 'vue'
-import { MdButton, MdContent, MdTabs } from 'vue-material/dist/components'
 import 'vue-material/dist/vue-material.min.css'
 import 'vue-material/dist/theme/default.css'
 
 Vue.use(VueMaterial)
 
-Vue.use(MdButton)
-Vue.use(MdContent)
-Vue.use(MdTabs)
 
 export default {
   name: 'App',
@@ -60,6 +50,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 5px;
+  margin-top: 2px;
 }
 </style>
