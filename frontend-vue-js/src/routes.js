ebe7af38b4816ab959f8a01a5ab3611e70734b28
vlaksi <daksrapsody@gmail.com>
2021-01-31 01:40:44 +0100

    feat: added page for registration & custom validation function in form

--- frontend-vue-js/src/routes.js
+++ frontend-vue-js/src/routes.js
@@ -5,14 +5,16 @@ import pharmacyadminLanding from './components/pharmacyadmin/Landingpage.vue';
 import pharmacistLanding from './components/pharmacist/Landingpage.vue';
 import patientLanding from './components/patient/Landingpage.vue';
 import dermatologistLanding from './components/dermatologist/Landingpage.vue';
+import register from './components/unregistered/RegisterPage.vue';
 
 export default [
     { path: '/', component: unregisterLanding },
-    { path: '/components/systemadmin/Landingpage.vue', component: systemadminLanding, name: 'systemadminLanding'},
-    { path: '/components/supplier/Landingpage.vue', component: supplierLanding, name: 'supplierLanding'},
-    { path: '/components/pharmacyadmin/Landingpage.vue', component: pharmacyadminLanding, name: 'pharmacyadminLanding'},
-    { path: '/components/pharmacist/Landingpage.vue', component: pharmacistLanding, name: 'pharmacistLanding'},
-    { path: '/components/patient/Landingpage.vue', component: patientLanding, name: 'patientLanding'},
-    { path: '/components/dermatologist/Landingpage.vue', component: dermatologistLanding, name: 'dermatologistLanding'},
+    { path: '/components/register', component: register, name:'registerPage' },
+    { path: '/components/systemadmin/Landingpage.vue', component: systemadminLanding, name: 'systemadminLanding' },
+    { path: '/components/supplier/Landingpage.vue', component: supplierLanding, name: 'supplierLanding' },
+    { path: '/components/pharmacyadmin/Landingpage.vue', component: pharmacyadminLanding, name: 'pharmacyadminLanding' },
+    { path: '/components/pharmacist/Landingpage.vue', component: pharmacistLanding, name: 'pharmacistLanding' },
+    { path: '/components/patient/Landingpage.vue', component: patientLanding, name: 'patientLanding' },
+    { path: '/components/dermatologist/Landingpage.vue', component: dermatologistLanding, name: 'dermatologistLanding' },
 ]
 
