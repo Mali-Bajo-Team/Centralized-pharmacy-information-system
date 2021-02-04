7556f1960c24e265615065f48be3f9d202fcc716
Pufke <pualic.nemanja@gmail.com>
2021-02-04 20:25:29 +0100

    feat: clicked event on claendar opet dialog and back event

--- frontend-vue-js/src/components/Calendar.vue
+++ frontend-vue-js/src/components/Calendar.vue
@@ -1,6 +1,5 @@
 <template>
   <v-app>
-
     <v-sheet tile height="54" class="d-flex">
       <v-btn icon class="ma-2" @click="$refs.calendar.prev()">
         <v-icon>mdi-chevron-left</v-icon>
@@ -48,14 +47,48 @@
         :event-overlap-threshold="30"
         :event-color="getEventColor"
         @change="getEvents"
+        @click:event="myTime"
       ></v-calendar>
     </v-sheet>
+
+    <!-- Start DIALOG-->
+    <v-dialog v-model="dialog" persistent max-width="600px">
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn color="primary" dark v-bind="attrs" v-on="on">
+          Open Dialog
+        </v-btn>
+      </template>
+      <v-card>
+        <v-card-title>
+          <span class="headline">User Profile</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container>
+            <v-row> </v-row>
+          </v-container>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="blue darken-1" text @click="dialog = false">
+            Close
+          </v-btn>
+          <v-btn color="blue darken-1" text @click="dialog = false">
+            Save
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+    <!-- End DIALOG-->
   </v-app>
 </template>
 
 <script>
 export default {
   data: () => ({
+    dragEvent: null,
+    
+    event: null,
+    dialog: false,
     consultants: [],
     response: null,
     type: "month",
@@ -83,6 +116,17 @@ export default {
     names: ["Pregled", "Holiday"],
   }),
   methods: {
+     startDrag ({ event, timed }) {
+        if (event && timed) {
+          this.dragEvent = event
+          this.dragTime = null
+          this.extendOriginal = null
+        }
+      },
+    myTime(event) {
+      this.dialog = true;
+      console.log(event);
+    },
     getEvents() {
       var token = parseJwt(localStorage.getItem("JWT-CPIS"));
       var email = token.sub;
@@ -90,7 +134,8 @@ export default {
       this.consultants = [];
       this.axios
         .post(
-          process.env.VUE_APP_BACKEND_URL + process.env.VUE_APP_CONSULTANTEXAMINATIONS_ENDPOINT,
+          process.env.VUE_APP_BACKEND_URL +
+            process.env.VUE_APP_CONSULTANTEXAMINATIONS_ENDPOINT,
           { email: email },
           {
             headers: {
