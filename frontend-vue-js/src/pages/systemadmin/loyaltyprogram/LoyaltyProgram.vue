d643486428f080328e48746c25f034f1217c5929
vlaksi <daksrapsody@gmail.com>
2021-02-05 21:28:49 +0100

    feat[3.40][frontend]: ajax call to get loyalty program

--- frontend-vue-js/src/pages/systemadmin/loyaltyprogram/LoyaltyProgram.vue
+++ frontend-vue-js/src/pages/systemadmin/loyaltyprogram/LoyaltyProgram.vue
@@ -1,13 +1,28 @@
 <template>
-    <v-container>
-        <h3>Loyalty Program</h3>
-    </v-container>
+  <v-container>
+    <h3>Loyalty Program</h3>
+  </v-container>
 </template>
 
 <script>
 export default {
-    data: () => ({
-
-    })
-}
+  data: () => ({
+      loyaltyProgram: null,
+  }),
+  mounted() {
+    this.axios
+      .get(
+        process.env.VUE_APP_BACKEND_URL +
+          process.env.VUE_APP_LOYALTY_PROGRAM,
+        {
+          headers: {
+            Authorization: "Bearer " + localStorage.getItem("JWT-CPIS"),
+          },
+        }
+      )
+      .then((resp) => {
+        this.loyaltyProgram = resp.data;
+      });
+  },
+};
 </script>
