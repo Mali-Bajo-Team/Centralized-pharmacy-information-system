4e7906f6cfbaedf659526c62646b17e390a1d1fb
Pufke <pualic.nemanja@gmail.com>
2021-02-05 20:18:08 +0100

    feat: isConsultantWorkingTime method

--- backend-spring-boot/src/main/java/com/pharmacy/cpis/scheduleservice/service/IWorkingTimesService.java
+++ backend-spring-boot/src/main/java/com/pharmacy/cpis/scheduleservice/service/IWorkingTimesService.java
@@ -2,10 +2,13 @@ package com.pharmacy.cpis.scheduleservice.service;
 
 import com.pharmacy.cpis.scheduleservice.model.workschedule.WorkingTimes;
 
+import java.util.Date;
 import java.util.List;
 
 public interface IWorkingTimesService {
     List<WorkingTimes> findAll();
 
     WorkingTimes consultantWorkingTime(Long consultantID);
+
+    Boolean isConsultantWorkingTime(Long consultantID, Date examinationTime);
 }
--- backend-spring-boot/src/main/java/com/pharmacy/cpis/scheduleservice/service/impl/WorkingTimesService.java
+++ backend-spring-boot/src/main/java/com/pharmacy/cpis/scheduleservice/service/impl/WorkingTimesService.java
@@ -6,6 +6,8 @@ import com.pharmacy.cpis.scheduleservice.service.IWorkingTimesService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -30,6 +32,50 @@ public class WorkingTimesService implements IWorkingTimesService {
             }
         }
         return  consultantWorkingTime;
+    }
+
+    @Override
+    public Boolean isConsultantWorkingTime(Long consultantID, Date examinationTime) {
+        WorkingTimes consultantWorkingTime = consultantWorkingTime(consultantID);
+
+        //number ranges from 1 (Sunday) to 7 (Saturday).
+        Integer numberOfWeekDay = getDayNumberOld(examinationTime);
+        if(numberOfWeekDay == 1){
+            if(examinationTime.getTime() >= consultantWorkingTime.getSunday().getStart().getTime() && examinationTime.getTime() < consultantWorkingTime.getSunday().getEnd().getTime()){
+                return true;
+            }
+        }else if(numberOfWeekDay == 2){
+            if(examinationTime.getTime() >= consultantWorkingTime.getMonday().getStart().getTime() && examinationTime.getTime() < consultantWorkingTime.getMonday().getEnd().getTime()){
+                return true;
+            }
+        }else if(numberOfWeekDay == 3){
+            if(examinationTime.getTime() >= consultantWorkingTime.getTuesday().getStart().getTime() && examinationTime.getTime() < consultantWorkingTime.getTuesday().getEnd().getTime()){
+                return true;
+            }
+        }else if(numberOfWeekDay == 4){
+            if(examinationTime.getTime() >= consultantWorkingTime.getWednesday().getStart().getTime() && examinationTime.getTime() < consultantWorkingTime.getWednesday().getEnd().getTime()){
+                return true;
+            }
+        }else if(numberOfWeekDay == 5){
+            if(examinationTime.getTime() >= consultantWorkingTime.getThursday().getStart().getTime() && examinationTime.getTime() < consultantWorkingTime.getThursday().getEnd().getTime()){
+                return true;
+            }
+        }else if(numberOfWeekDay == 6){
+            if(examinationTime.getTime() >= consultantWorkingTime.getFriday().getStart().getTime() && examinationTime.getTime() < consultantWorkingTime.getFriday().getEnd().getTime()){
+                return true;
+            }
+        }else if(numberOfWeekDay == 7){
+            if(examinationTime.getTime() >= consultantWorkingTime.getSaturday().getStart().getTime() && examinationTime.getTime() < consultantWorkingTime.getSaturday().getEnd().getTime()){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static int getDayNumberOld(Date date) {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        return cal.get(Calendar.DAY_OF_WEEK);
     }
 }
 
