88567246b037191edb77383df0f9e3dacdd1f36b
Pufke <pualic.nemanja@gmail.com>
2021-02-06 22:50:37 +0100

    feat[3.11][front] initial table for examinited patients

--- frontend-vue-js/src/pages/pharmacist/landing/Landing.vue
+++ frontend-vue-js/src/pages/pharmacist/landing/Landing.vue
@@ -3,6 +3,29 @@
     <v-row>
       <v-col>
         <v-row>
+          <!-- SEARCH EXAMINITED USER -->
+          <div class="mb-16">
+               <h1 class="ml-n primary--text">Examinited patients</h1>
+            <v-card>
+              <v-card-title>
+                <v-text-field
+                  v-model="searchExaminitedUsers"
+                  append-icon="mdi-magnify"
+                  label="Search"
+                  single-line
+                  hide-details
+                ></v-text-field>
+              </v-card-title>
+              <v-data-table
+                :headers="headers"
+                :items="desserts"
+                :search="search"
+              ></v-data-table>
+            </v-card>
+          </div>
+        </v-row>
+       
+        <v-row>
           <h1 class="ml-n primary--text">Schedule consultation</h1>
           <!-- SEARCH USER -->
           <div class="ml-4 mb-6 mt-15">
@@ -167,8 +190,8 @@
           >
             The schedule must match the working hours of the pharmacist. The
             appointment should not be prepared with another examination or
-            consultation that the patient has scheduled (in any pharmacy). 
-            And also it is not possible to schedule consultations in the past.
+            consultation that the patient has scheduled (in any pharmacy). And
+            also it is not possible to schedule consultations in the past.
           </v-alert>
         </v-row>
       </v-col>
@@ -197,6 +220,102 @@ export default {
     selectedPatient: null,
     scheduleAlert: false,
     scheduleSucces: false,
+    searchExaminitedUsers: "",
+    headers: [
+      {
+        text: "Dessert (100g serving)",
+        align: "start",
+        filterable: false,
+        value: "name",
+      },
+      { text: "Calories", value: "calories" },
+      { text: "Fat (g)", value: "fat" },
+      { text: "Carbs (g)", value: "carbs" },
+      { text: "Protein (g)", value: "protein" },
+      { text: "Iron (%)", value: "iron" },
+    ],
+    desserts: [
+      {
+        name: "Frozen Yogurt",
+        calories: 159,
+        fat: 6.0,
+        carbs: 24,
+        protein: 4.0,
+        iron: "1%",
+      },
+      {
+        name: "Ice cream sandwich",
+        calories: 237,
+        fat: 9.0,
+        carbs: 37,
+        protein: 4.3,
+        iron: "1%",
+      },
+      {
+        name: "Eclair",
+        calories: 262,
+        fat: 16.0,
+        carbs: 23,
+        protein: 6.0,
+        iron: "7%",
+      },
+      {
+        name: "Cupcake",
+        calories: 305,
+        fat: 3.7,
+        carbs: 67,
+        protein: 4.3,
+        iron: "8%",
+      },
+      {
+        name: "Gingerbread",
+        calories: 356,
+        fat: 16.0,
+        carbs: 49,
+        protein: 3.9,
+        iron: "16%",
+      },
+      {
+        name: "Jelly bean",
+        calories: 375,
+        fat: 0.0,
+        carbs: 94,
+        protein: 0.0,
+        iron: "0%",
+      },
+      {
+        name: "Lollipop",
+        calories: 392,
+        fat: 0.2,
+        carbs: 98,
+        protein: 0,
+        iron: "2%",
+      },
+      {
+        name: "Honeycomb",
+        calories: 408,
+        fat: 3.2,
+        carbs: 87,
+        protein: 6.5,
+        iron: "45%",
+      },
+      {
+        name: "Donut",
+        calories: 452,
+        fat: 25.0,
+        carbs: 51,
+        protein: 4.9,
+        iron: "22%",
+      },
+      {
+        name: "KitKat",
+        calories: 518,
+        fat: 26.0,
+        carbs: 65,
+        protein: 7,
+        iron: "6%",
+      },
+    ],
   }),
   methods: {
     handleSelectItem(item) {
@@ -266,7 +385,7 @@ export default {
             this.errorMessage = error.message;
             console.error("There was an error!", error);
             this.scheduleAlert = true;
-             this.scheduleSucces = false;
+            this.scheduleSucces = false;
           });
       }
     },
