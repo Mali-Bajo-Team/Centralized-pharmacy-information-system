e503a74165fc7bd4c65b1c4b05937ee098b0f309
vlaksi <daksrapsody@gmail.com>
2021-02-06 17:30:10 +0100

    feat[3.34]: init page for drugs preview

---
+++ frontend-vue-js/src/pages/guest/drugs/Drugs.vue
@@ -0,0 +1,20 @@
+<template>
+    <v-container>
+        <h1> Hi from drugs {{ test}} </h1>
+    </v-container>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+     test: "cao",
+    };
+  },
+  methods: {
+ 
+    
+  },
+};
+</script>
