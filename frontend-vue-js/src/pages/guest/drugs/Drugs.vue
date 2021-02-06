e670382418ef2f3311a01e903f19f79a91752645
vlaksi <daksrapsody@gmail.com>
2021-02-06 17:40:53 +0100

    feat[3.34]: ajax call to get all drugs

--- frontend-vue-js/src/pages/guest/drugs/Drugs.vue
+++ frontend-vue-js/src/pages/guest/drugs/Drugs.vue
@@ -1,20 +1,35 @@
 <template>
     <v-container>
-        <h1> Hi from drugs {{ test}} </h1>
+        <v-row>
+            <!-- Left column for filter & search -->
+            <v-col>
+                <p> Left </p>
+            </v-col>
+            <!-- End of the left column for filter & search -->
+
+            <!-- Right column for drugs preview-->
+            <v-col>
+                <p> Right </p>
+            </v-col>
+            <!-- End of the right colum for drugs preview-->
+        </v-row>
     </v-container>
 </template>
 
 <script>
 
 export default {
-  data() {
-    return {
-     test: "cao",
-    };
+  data: () => ({
+    drugs: [],
+  }),
+  mounted() {
+    this.axios
+      .get(process.env.VUE_APP_BACKEND_URL + process.env.VUE_APP_ALL_DRUGS_ENDPOINT,)
+      .then((resp) => {
+        this.drugs = resp.data;
+      });
   },
-  methods: {
- 
-    
-  },
+
+
 };
 </script>
