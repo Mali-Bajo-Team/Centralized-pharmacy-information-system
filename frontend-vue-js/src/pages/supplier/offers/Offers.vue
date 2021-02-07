eac8ef5101338eca32e9a287520e1146e2b06adb
vlaksi <daksrapsody@gmail.com>
2021-02-07 20:59:11 +0100

    feat[3.7][frontend]: form for filter by offer status

--- frontend-vue-js/src/pages/supplier/offers/Offers.vue
+++ frontend-vue-js/src/pages/supplier/offers/Offers.vue
@@ -1,6 +1,36 @@
 <template>
   <v-container>
-    <h3> Offers </h3>
+    <v-row>
+      <!-- Left column for filter & search -->
+      <v-col xl="4" sm="12" md="4">
+        <!-- Filter card -->
+        <v-card class="mt-5">
+          <!--Toolbar of the card-->
+          <v-toolbar color="primary" dark dense flat>
+            <v-toolbar-title class="body-2"> Filter offers </v-toolbar-title>
+          </v-toolbar>
+          <!--End of toolbar of the card-->
+<br>
+          <v-form class="pa-2">
+            <v-select
+              class="ml-4 mr-4"
+              
+              v-model="selectedOfferStatus"
+              :items="allOfferStatus"
+              label="Filter by offer status"
+              chips
+            >
+            </v-select>
+            <br>
+          </v-form>
+        </v-card>
+        <!-- End of the filter card -->
+      </v-col>
+      <!-- End of the left column for filter & search -->
+      <!-- Right column for drugs preview-->
+      <v-col xl="8" sm="12" md="8"> </v-col>
+      <!-- End of the right colum for drugs preview-->
+    </v-row>
   </v-container>
 </template>
 
@@ -8,12 +38,15 @@
 export default {
   data: () => ({
     supplierOffers: [],
+    selectedOfferStatus: "",
+    allOfferStatus: ["PENDING", "ACCEPTED", "REJECTED"],
   }),
   methods: {},
   mounted() {
     this.axios
       .get(
-        process.env.VUE_APP_BACKEND_URL + process.env.VUE_APP_PROCUREMENT_OFFERS_ENDPOINT,
+        process.env.VUE_APP_BACKEND_URL +
+          process.env.VUE_APP_PROCUREMENT_OFFERS_ENDPOINT,
         {
           headers: {
             Authorization: "Bearer " + localStorage.getItem("JWT-CPIS"),
@@ -21,8 +54,8 @@ export default {
         }
       )
       .then((resp) => {
-        alert("success");
-        this.supplierOffers = resp.data[0];
+        // alert("success");
+        this.supplierOffers = resp.data;
       });
   },
 };
