4eaa85ce334dc14a9af7001593b00a6f6b81a6b7
milana-todorovic <milanatodorovic@uns.ac.rs>
2021-02-08 16:54:06 +0100

    feat[3.26.1]: added endpoint for accepting a drug order offer

--- backend-spring-boot/src/main/java/com/pharmacy/cpis/drugservice/service/impl/OfferService.java
+++ backend-spring-boot/src/main/java/com/pharmacy/cpis/drugservice/service/impl/OfferService.java
@@ -10,12 +10,21 @@ import com.pharmacy.cpis.drugservice.model.drugprocurement.DrugOrder;
 import com.pharmacy.cpis.drugservice.model.drugprocurement.DrugOrderStatus;
 import com.pharmacy.cpis.drugservice.model.drugprocurement.Offer;
 import com.pharmacy.cpis.drugservice.model.drugprocurement.OfferStatus;
+import com.pharmacy.cpis.drugservice.model.drugprocurement.OrderedDrug;
+import com.pharmacy.cpis.drugservice.model.drugsales.AvailableDrug;
+import com.pharmacy.cpis.drugservice.repository.IAvailableDrugRepository;
 import com.pharmacy.cpis.drugservice.repository.IDrugOrderRepository;
 import com.pharmacy.cpis.drugservice.repository.IOfferRepository;
 import com.pharmacy.cpis.drugservice.service.IOfferService;
+import com.pharmacy.cpis.userservice.model.users.PharmacyAdministrator;
 import com.pharmacy.cpis.userservice.model.users.Supplier;
+import com.pharmacy.cpis.userservice.model.users.UserAccount;
+import com.pharmacy.cpis.userservice.repository.IPharmacyAdministratorRepository;
+import com.pharmacy.cpis.userservice.service.EmailService;
 import com.pharmacy.cpis.userservice.service.ISupplierService;
 import com.pharmacy.cpis.util.exceptions.PSBadRequestException;
+import com.pharmacy.cpis.util.exceptions.PSConflictException;
+import com.pharmacy.cpis.util.exceptions.PSForbiddenException;
 import com.pharmacy.cpis.util.exceptions.PSNotFoundException;
 
 @Service
@@ -29,6 +38,15 @@ public class OfferService implements IOfferService {
 
 	@Autowired
 	private ISupplierService supplierService;
+
+	@Autowired
+	private IPharmacyAdministratorRepository pharmacyAdminRepository;
+
+	@Autowired
+	private IAvailableDrugRepository availableDrugRepository;
+
+	@Autowired
+	private EmailService emailService;
 
 	@Override
 	public List<Offer> findOffersBySupplier(Supplier supplier) {
@@ -65,5 +83,58 @@ public class OfferService implements IOfferService {
 
 		drugOrderRepository.save(order);
 		return offerRepository.save(offer);
+	}
+
+	public void accept(UserAccount user, Long offerId) {
+		PharmacyAdministrator admin = pharmacyAdminRepository.findByAccount(user).orElse(null);
+		if (admin == null)
+			throw new PSForbiddenException("No pharmacy administrator associated with this account.");
+
+		Offer offerToAccept = offerRepository.findById(offerId).orElse(null);
+		if (offerToAccept == null)
+			throw new PSNotFoundException("The requested offer does not exist.");
+
+		DrugOrder order = offerToAccept.getOrder();
+
+		if (!order.getAdministrator().equals(admin))
+			throw new PSForbiddenException("Offers can only be accepted by the order's creator.");
+
+		if (order.getStatus().equals(DrugOrderStatus.FINISHED))
+			throw new PSConflictException("The requested drug order is already finished.");
+
+		order.setStatus(DrugOrderStatus.FINISHED);
+		drugOrderRepository.save(order);
+		updateAmounts(order);
+		updateOffers(order, offerToAccept);
+	}
+
+	private void updateAmounts(DrugOrder order) {
+		for (OrderedDrug drug : order.getOrderedDrugs()) {
+			AvailableDrug availableDrug = availableDrugRepository
+					.findByPharmacyIdAndDrugCode(order.getPharmacy().getId(), drug.getDrug().getCode()).orElse(null);
+
+			if (availableDrug == null)
+				throw new PSConflictException(
+						"Drug with code " + drug.getDrug().getCode() + " is not available in the pharmacy.");
+
+			availableDrug.setAvailableAmount(availableDrug.getAvailableAmount() + drug.getAmount());
+			availableDrugRepository.save(availableDrug);
+		}
+	}
+
+	private void updateOffers(DrugOrder order, Offer offerToAccept) {
+		for (Offer offer : order.getOffers()) {
+			if (offer.equals(offerToAccept))
+				offer.setStatus(OfferStatus.ACCEPTED);
+			else
+				offer.setStatus(OfferStatus.REJECTED);
+
+			offerRepository.save(offer);
+			try {
+				emailService.sendOfferUpdateEmailAsync(offer.getSupplier().getAccount().getEmail(), offer);
+			} catch (Exception e) {
+				System.out.println("Email sending failed!");
+			}
+		}
 	}
 }
