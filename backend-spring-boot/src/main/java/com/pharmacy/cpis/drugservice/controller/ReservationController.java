2ee80eb64ce881785d68ae5d1380a7974d7d8ee9
Ana Perisic <anaperisictb@yahoo.com>
2021-02-09 18:37:38 +0100

    feat[3.19][frontend]:added axios, done validations of date and amount, added authorization

--- backend-spring-boot/src/main/java/com/pharmacy/cpis/drugservice/controller/ReservationController.java
+++ backend-spring-boot/src/main/java/com/pharmacy/cpis/drugservice/controller/ReservationController.java
@@ -5,9 +5,11 @@ import com.pharmacy.cpis.drugservice.dto.DrugReservationDTO;
 import com.pharmacy.cpis.drugservice.model.drugsales.Reservation;
 import com.pharmacy.cpis.drugservice.dto.ReservationDTO;
 import com.pharmacy.cpis.drugservice.service.IReservationService;
+import com.pharmacy.cpis.util.aspects.EmployeeAccountActive;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,11 +21,15 @@ public class ReservationController {
     @Autowired
     private IReservationService reservationService;
 
+    @PreAuthorize("hasRole('PATIENT')")
     @PostMapping(value = "/drug")
     public ResponseEntity<DrugReservationDTO> makeReservationOfDrug(@RequestBody DrugReservationDTO drugReservationDTO){
         Reservation reservation = reservationService.saveReservation(drugReservationDTO);
         return new ResponseEntity<>(new DrugReservationDTO(reservation) , HttpStatus.OK);
     }
+
+    @PreAuthorize("hasRole('DERMATOLOGIST')")
+    @EmployeeAccountActive
     @PostMapping(value = "/isvalid")
     public ResponseEntity<ReservationDTO> getDrugPharmaciesPrices(@RequestBody ReservationDTO reservationDTO){
 
@@ -32,6 +38,8 @@ public class ReservationController {
         return new ResponseEntity<>(reservationDTOforReturn, HttpStatus.OK);
     }
 
+    @PreAuthorize("hasRole('DERMATOLOGIST')")
+    @EmployeeAccountActive
     @PostMapping(value = "/dispense")
     public ResponseEntity<ReservationDTO> dispensingMedicine(@RequestBody ReservationDTO reservationDTO){
 
