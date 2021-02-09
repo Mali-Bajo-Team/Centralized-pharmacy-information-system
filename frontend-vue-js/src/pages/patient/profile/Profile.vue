c65951438f81fff01942762cd986606b13f335c1
vlaksi <daksrapsody@gmail.com>
2021-02-09 03:20:55 +0100

    feat[3.39][frontend]: added complaint design to patient profile

--- frontend-vue-js/src/pages/patient/profile/Profile.vue
+++ frontend-vue-js/src/pages/patient/profile/Profile.vue
@@ -1,131 +1,172 @@
 <template >
-  <v-card width="600px" class="mx-auto" v-if="patient">
-    <v-card-title>{{ patient.name }} {{ patient.surname }} </v-card-title>
-    <v-row v-if="patient.userCategoryDTO">
-      <v-col xl="8" md="8" sm="12">
-        <v-card-text>
-          <!--Username-->
-          <v-icon color="primary">mdi-account</v-icon>
-          {{ patient.email }}
-          <br /><br />
+  <v-container v-if="patient">
+    <!-- Card for the base info about patient -->
+    <v-card width="600px" class="mx-auto">
+      <v-card-title>{{ patient.name }} {{ patient.surname }} </v-card-title>
+      <v-row v-if="patient.userCategoryDTO">
+        <v-col xl="8" md="8" sm="12">
+          <v-card-text>
+            <!--Username-->
+            <v-icon color="primary">mdi-account</v-icon>
+            {{ patient.email }}
+            <br /><br />
 
-          <!--Adress-->
-          <v-icon color="primary"> mdi-map-marker </v-icon>
-          {{ patient.address }}
-          <br /><br />
+            <!--Adress-->
+            <v-icon color="primary"> mdi-map-marker </v-icon>
+            {{ patient.address }}
+            <br /><br />
 
-          <!--City-->
-          <v-icon color="primary">mdi-city</v-icon>
-          {{ patient.city }}
-          <br /><br />
+            <!--City-->
+            <v-icon color="primary">mdi-city</v-icon>
+            {{ patient.city }}
+            <br /><br />
 
-          <!--Country-->
-          <v-icon color="primary">mdi-map</v-icon>
-          {{ patient.country }}
-          <br /><br />
+            <!--Country-->
+            <v-icon color="primary">mdi-map</v-icon>
+            {{ patient.country }}
+            <br /><br />
 
-          <!--Phone nubmer-->
-          <v-icon color="primary"> mdi-phone-classic </v-icon>
-          {{ patient.phoneNumber }}
+            <!--Phone nubmer-->
+            <v-icon color="primary"> mdi-phone-classic </v-icon>
+            {{ patient.phoneNumber }}
 
-          <br /><br />
-          <v-divider></v-divider>
-          <br />
-
-          <!--Points-->
-          <v-icon color="primary">mdi-star</v-icon>
-          {{ patient.loyaltyPoints }} <i> points</i> <br /><br />
-
-          <!--Category-->
-          <v-chip color="primary">{{patient.userCategoryDTO.name}} </v-chip>
-          <br /><br />
-
-          <!--Benefits-->
-
-          <v-chip color="primary"
-            >Reservation discount:{{
-              patient.userCategoryDTO.reservationDiscount
-            }}
-            %</v-chip
-          >
-          <br /><br />
-
-          <v-chip color="primary"
-            >Consultation discount:{{
-              patient.userCategoryDTO.consultationDiscount
-            }}
-            %</v-chip
-          >
-          <b> </b>
-        </v-card-text>
-      </v-col>
-
-      <!--Dialog form-->
-      <v-col xl="4" md="4" sm="12">
-        <v-dialog width="500">
-          <template v-slot:activator="{ on, attrs }">
-            <v-btn fab dark large color="primary" v-bind="attrs" v-on="on">
-              <v-icon dark> mdi-pencil </v-icon>
-            </v-btn>
-          </template>
-
-          <v-card>
-            <v-toolbar color="primary" dark dense flat>
-              <v-toolbar-title class="body-2">
-                Change your personal information
-              </v-toolbar-title>
-            </v-toolbar>
-            <v-card-text>
-              <v-form>
-                <v-text-field
-                  label="Change your name "
-                  v-model="patientFormDTO.name"
-                ></v-text-field>
-                <v-text-field
-                  label="Change your surname "
-                  v-model="patientFormDTO.surname"
-                ></v-text-field>
-                <v-text-field
-                  label="Change your address"
-                  v-model="patientFormDTO.address"
-                ></v-text-field>
-                <v-text-field
-                  label="Change your city"
-                  v-model="patientFormDTO.city"
-                ></v-text-field>
-                <v-text-field
-                  label="Change your country"
-                  v-model="patientFormDTO.country"
-                ></v-text-field>
-                <v-text-field
-                  label="Change your phone number"
-                  v-model="patientFormDTO.phoneNumber"
-                ></v-text-field>
-                <v-text-field
-                  label="Add drugs which cause you allergic reactions"
-                  v-model="patientFormDTO.allergies"
-                ></v-text-field>
-              </v-form>
-            </v-card-text>
-
+            <br /><br />
             <v-divider></v-divider>
+            <br />
 
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn color="primary" text @click="dialog = false">
-                Save changes
+            <!--Points-->
+            <v-icon color="primary">mdi-star</v-icon>
+            {{ patient.loyaltyPoints }} <i> points</i> <br /><br />
+
+            <!--Category-->
+            <v-chip color="primary">{{ patient.userCategoryDTO.name }} </v-chip>
+            <br /><br />
+
+            <!--Benefits-->
+
+            <v-chip color="primary"
+              >Reservation discount:{{
+                patient.userCategoryDTO.reservationDiscount
+              }}
+              %</v-chip
+            >
+            <br /><br />
+
+            <v-chip color="primary"
+              >Consultation discount:{{
+                patient.userCategoryDTO.consultationDiscount
+              }}
+              %</v-chip
+            >
+            <b> </b>
+          </v-card-text>
+        </v-col>
+
+        <!--Dialog form-->
+        <v-col xl="4" md="4" sm="12">
+          <v-dialog width="500">
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn fab dark large color="primary" v-bind="attrs" v-on="on">
+                <v-icon dark> mdi-pencil </v-icon>
               </v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-dialog>
-      </v-col>
-    </v-row>
-  </v-card>
+            </template>
+
+            <v-card>
+              <v-toolbar color="primary" dark dense flat>
+                <v-toolbar-title class="body-2">
+                  Change your personal information
+                </v-toolbar-title>
+              </v-toolbar>
+              <v-card-text>
+                <v-form>
+                  <v-text-field
+                    label="Change your name "
+                    v-model="patientFormDTO.name"
+                  ></v-text-field>
+                  <v-text-field
+                    label="Change your surname "
+                    v-model="patientFormDTO.surname"
+                  ></v-text-field>
+                  <v-text-field
+                    label="Change your address"
+                    v-model="patientFormDTO.address"
+                  ></v-text-field>
+                  <v-text-field
+                    label="Change your city"
+                    v-model="patientFormDTO.city"
+                  ></v-text-field>
+                  <v-text-field
+                    label="Change your country"
+                    v-model="patientFormDTO.country"
+                  ></v-text-field>
+                  <v-text-field
+                    label="Change your phone number"
+                    v-model="patientFormDTO.phoneNumber"
+                  ></v-text-field>
+                  <v-text-field
+                    label="Add drugs which cause you allergic reactions"
+                    v-model="patientFormDTO.allergies"
+                  ></v-text-field>
+                </v-form>
+              </v-card-text>
+
+              <v-divider></v-divider>
+
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="primary" text @click="dialog = false">
+                  Save changes
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+        </v-col>
+      </v-row>
+    </v-card>
+    <!-- End of the card for the base info about patient -->
+    <!-- Card for the complaints -->
+    <v-card width="600px" class="mx-auto">
+      <v-row justify="end">
+      <v-card-subtitle class="ml-5 mr-5">
+        Dear {{ patient.name }}, if you have some complaint on some
+        pharmacist/dermatologist or even a pharmacist, fell free to inform us.
+      </v-card-subtitle>
+
+      <v-card-actions class="mr-2 mb-2">
+        <v-btn color="orange lighten-2" text> Make complain </v-btn>
+
+        <v-spacer></v-spacer>
+
+        <v-btn class="mr-5" icon @click="showComplaintPart = !showComplaintPart">
+          <v-icon>{{
+            showComplaintPart ? "mdi-chevron-up" : "mdi-chevron-down"
+          }}</v-icon>
+        </v-btn>
+      </v-card-actions>
+
+      <v-expand-transition>
+        <div class="pa-3" v-show="showComplaintPart">
+          <v-divider></v-divider>
+
+          <v-card-text>
+            I'm a thing. But, like most politicians, he promised more than he
+            could deliver. You won't have time for sleeping, soldier, not with
+            all the bed making you'll be doing. Then we'll go with that data
+            file! Hey, you add a one and two zeros to that or we walk! You're
+            going to do his laundry? I've got to find a way to escape.
+          </v-card-text>
+        </div>
+      </v-expand-transition>
+      </v-row>
+    </v-card>
+    <!-- End of the card for the complaints -->
+  </v-container>
 </template>
 <script>
 import { getParsedToken } from "./../../../util/token";
 export default {
   data: () => ({
+    showComplaintPart: false,
     patient: {},
     patientFormDTO: {
       name: "",
@@ -134,7 +175,7 @@ export default {
       city: "",
       country: "",
       phoneNumber: "",
-      allergies:""
+      allergies: "",
     },
     rules: {
       required: (value) => !!value || "Required.",
@@ -164,7 +205,6 @@ export default {
         this.patientFormDTO.city = this.patient.city;
         this.patientFormDTO.country = this.patient.country;
         this.patientFormDTO.phoneNumber = this.patient.phoneNumber;
-       
       })
       .catch((error) => {
         alert("Error: " + error);
