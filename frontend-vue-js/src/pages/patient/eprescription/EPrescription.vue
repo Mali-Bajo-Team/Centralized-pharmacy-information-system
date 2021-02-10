f4521b8813e540f12d70509ab259b6ed84fac9b4
vlaksi <daksrapsody@gmail.com>
2021-02-10 09:24:40 +0100

    feat[3.35]: finded pharmacy show up

--- frontend-vue-js/src/pages/patient/eprescription/EPrescription.vue
+++ frontend-vue-js/src/pages/patient/eprescription/EPrescription.vue
@@ -21,7 +21,98 @@
       <!-- Left column for qr-code-upload, filter & search -->
 
       <!-- Right column for pharmacies -->
-      <v-col xl="8" sm="12" md="8"> right side </v-col>
+      <v-col xl="8" sm="12" md="8">
+        <v-card
+          elevation="4"
+          class="pa-4 ml-10 mb-10"
+          v-for="pharmacy in pharmaciesWithRequiredDrugsAmount"
+          :key="pharmacy.pharmacyId"
+        >
+          <!-- Row for title, location & buy confirmation dialog -->
+          <v-row align="center">
+            <v-card-subtitle>
+              <h3>{{ pharmacy.pharmacyName }}</h3>
+            </v-card-subtitle>
+            <v-spacer></v-spacer>
+            <!-- Buy confirmation dialog -->
+            <v-dialog
+              v-model="pharmacy.showConfirmDialog"
+              width="500"
+              :retain-focus="false"
+            >
+              <template #activator="{ on: dialog }">
+                <v-tooltip bottom>
+                  <template #activator="{ on: tooltip }">
+                    <v-btn
+                      v-on="{ ...tooltip, ...dialog }"
+                      elevation="0"
+                      left
+                      class="mr-10 ml-3"
+                      fab
+                      dark
+                      x-small
+                      color="green darken-2"
+                      ><v-icon dark> mdi-basket </v-icon>
+                    </v-btn>
+                  </template>
+                  <span>Buy</span>
+                </v-tooltip>
+              </template>
+              <v-card>
+                <!--Toolbar of the card-->
+                <v-toolbar color="primary" dark dense flat>
+                  <v-toolbar-title class="body-2">
+                    <h3>Confirmation</h3>
+                  </v-toolbar-title>
+                </v-toolbar>
+                <!-- End of toolbar of the card -->
+                <br />
+                <v-form class="ma-5">
+                  <p>cao</p>
+                </v-form>
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn
+                    color="primary"
+                    @click="
+                      pharmacy.showConfirmDialog = !pharmacy.showConfirmDialog
+                    "
+                  >
+                    Close
+                  </v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
+            <!-- End of the buy confirmation dialog  -->
+          </v-row>
+          <!-- End of the row for title, location & buy confirmation dialog  -->
+          <v-row class="mb-1">
+            <v-chip
+              small
+              class="ma-2 ml-4"
+              color="teal"
+              text-color="white"
+              pill
+            >
+              <v-icon small left> mdi-pill </v-icon>
+
+              {{ pharmacy.pharmacyLocation }}
+            </v-chip>
+            <v-spacer></v-spacer>
+            <!-- Mark of drug -->
+            <v-rating
+              class="mr-10"
+              v-model="pharmacy.pharmacyRating"
+              color="accent"
+              background-color="orange "
+              half-increments
+              readonly
+            >
+            </v-rating>
+            <!-- End of mark of the drug -->
+          </v-row>
+        </v-card>
+      </v-col>
       <!-- End of the right column for the pharmacies -->
     </v-row>
   </v-container>
@@ -55,7 +146,18 @@ export default {
           }
         )
         .then((resp) => {
-          this.pharmaciesWithRequiredDrugsAmount = resp.data;
+          this.pharmaciesWithRequiredDrugsAmount = [];
+          for (let pharmacy of resp.data) {
+            let tempObj = {
+              showConfirmDialog: false,
+              pharmacyId: pharmacy.pharmacyId,
+              totalPrice: pharmacy.totalPrice,
+              pharmacyRating: pharmacy.pharmacyRating,
+              pharmacyName: pharmacy.pharmacyName,
+              pharmacyLocation: pharmacy.pharmacyLocation,
+            };
+            this.pharmaciesWithRequiredDrugsAmount.push(tempObj);
+          }
         })
         .catch((error) => {
           alert("Error: " + error);
