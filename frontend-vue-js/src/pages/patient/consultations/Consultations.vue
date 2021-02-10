8e64e2413310946944e0bc74026ca6c81c2adaab
Ana Perisic <anaperisictb@yahoo.com>
2021-02-10 23:10:40 +0100

    feat[3.15]: axios to get patient consultations

--- frontend-vue-js/src/pages/patient/consultations/Consultations.vue
+++ frontend-vue-js/src/pages/patient/consultations/Consultations.vue
@@ -5,7 +5,58 @@
 </template>
 
 <script>
+import { getParsedToken } from "./../../../util/token";
 export default {
-  data: () => ({}),
+  data: () => ({
+    patientConsultations: [],
+  }),
+  methods: {},
+  mounted() {
+    this.axios
+      .post(
+        process.env.VUE_APP_BACKEND_URL +
+          process.env.VUE_APP_PATIENT_DERMATOLOGIST_CONSULTATION_ENDPOINT,
+        {
+          email: getParsedToken().sub,
+        },
+        {
+          headers: {
+            Authorization: "Bearer " + localStorage.getItem("JWT-CPIS"),
+          },
+        }
+      )
+      .then((resp) => {
+        console.log("success get of the patient dermatologist consultations");
+        for (let consultation of resp.data) {
+          this.patientConsultations.push(consultation);
+        }
+      })
+      .catch((error) => {
+        alert(error);
+      });
+
+    this.axios
+      .post(
+        process.env.VUE_APP_BACKEND_URL +
+          process.env.VUE_APP_PATIENT_PHARMACIST_CONSULTATION_ENDPOINT,
+        {
+          email: getParsedToken().sub,
+        },
+        {
+          headers: {
+            Authorization: "Bearer " + localStorage.getItem("JWT-CPIS"),
+          },
+        }
+      )
+      .then((resp) => {
+        console.log("success get of the patient pharmacist consultations");
+        for (let consultation of resp.data) {
+          this.patientConsultations.push(consultation);
+        }
+      })
+      .catch((error) => {
+        alert(error);
+      });
+  },
 };
 </script>
