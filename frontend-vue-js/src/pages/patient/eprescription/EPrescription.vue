9131af3cf2bad4d36abc8bd06d65af10b73d69de
vlaksi <daksrapsody@gmail.com>
2021-02-10 05:55:28 +0100

    feat[3.35][frontend]: design for qr-code insert

--- frontend-vue-js/src/pages/patient/eprescription/EPrescription.vue
+++ frontend-vue-js/src/pages/patient/eprescription/EPrescription.vue
@@ -1,13 +1,30 @@
 <template>
-<v-container>
-  <div>
-    <p class="decode-result">
-      Last result: <b>{{ result }}</b>
-    </p>
+  <v-container>
+    <v-row>
+      <!-- Left column for qr-code-upload, filter & search -->
+      <v-col xl="4" sm="12" md="4">
+        <!-- Upload QR code card -->
+        <v-card>
+          <!--Toolbar of the card-->
+          <v-toolbar color="primary" dark dense flat>
+            <v-toolbar-title class="body-2"> Search drugs </v-toolbar-title>
+          </v-toolbar>
+          <!--End of the toolbar of the card-->
 
-    <qrcode-capture @decode="onDecode" />
-  </div>
-</v-container>
+          <v-card-subtitle class="ma-1" > Upload your QR code</v-card-subtitle>
+          <v-form class="pa-5">
+            <qrcode-capture @decode="onDecode" />
+          </v-form>
+        </v-card>
+        <!--End of the upload of the QR code card -->
+      </v-col>
+      <!-- Left column for qr-code-upload, filter & search -->
+
+      <!-- Right column for pharmacies -->
+      <v-col xl="8" sm="12" md="8"> right side </v-col>
+      <!-- End of the right column for the pharmacies -->
+    </v-row>
+  </v-container>
 </template>
 
 <script>
@@ -24,6 +41,7 @@ export default {
   methods: {
     onDecode(result) {
       this.result = result;
+      console.log(result);
     },
   },
 };
