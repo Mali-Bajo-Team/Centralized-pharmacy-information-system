46dd5048e1db1d55a4e9f26237d947d39272389a
Ana Perisic <anaperisictb@yahoo.com>
2021-02-11 18:24:26 +0100

    feat[3.9]: added axios calls for getting my EPrescriptions

--- frontend-vue-js/src/pages/patient/eprescription/MyEPrescriptions.vue
+++ frontend-vue-js/src/pages/patient/eprescription/MyEPrescriptions.vue
@@ -1,9 +1,9 @@
 <template>
-    <v-container>
-        <v-row>
-        <!--Left column-->
-        <v-col xl="4" sm="12" md="4">
-            <v-card mt="5">
+  <v-container>
+    <v-row>
+      <!--Left column-->
+      <v-col xl="4" sm="12" md="4">
+        <v-card mt="5">
           <v-toolbar color="primary" dark dense flat>
             <v-toolbar-title class="body-2">
               Sort my EPrescriptions
@@ -17,7 +17,6 @@
               width="110px"
               color="green lighten-2"
               class="ma-2 white--text mr-6"
-              
             >
               Date
               <v-icon small right dark> mdi-arrow-up-bold </v-icon>
@@ -27,17 +26,16 @@
               width="110px"
               color="green lighten-2"
               class="ma-2 white--text ml-6"
-             
             >
               Date
               <v-icon small right dark> mdi-arrow-down-bold </v-icon>
             </v-btn>
           </v-row>
-          </v-card>
-          <!-- End of the sort by date -->
-          <br><br>
-          <!--Toolbar of the card-->
-          <v-card>
+        </v-card>
+        <!-- End of the sort by date -->
+        <br /><br />
+        <!--Toolbar of the card-->
+        <v-card>
           <v-toolbar color="primary" dark dense flat>
             <v-toolbar-title class="body-2">
               Filter my EPrescriptions by status
@@ -47,33 +45,60 @@
           <!--End of toolbar of the card-->
 
           <!--Filter form-->
-          <v-card-text>
-            
-            
-          </v-card-text>
-          </v-card>
-       
+          <v-card-text> </v-card-text>
+        </v-card>
+
         <!--End of filter form-->
         <br /><br />
-       
-        </v-col>
-        <!--End of left column-->
+      </v-col>
+      <!--End of left column-->
 
-        <!--Right column-->
+      <!--Right column-->
       <v-col xl="8" sm="12" md="8">
         <v-card
-        
+          class="pa-2 ml-16 mr-16 mb-10"
+          v-for="prescription in prescriptions"
+          :key="prescription.prescriptionId"
         >
           <v-card-subtitle>
             <h3 class="ml-3">
-              
-              ana
+              {{ prescription.status }}
             </h3>
           </v-card-subtitle>
           <v-spacer></v-spacer>
         </v-card>
       </v-col>
       <!--End of right column-->
-      </v-row>
-    </v-container>
+    </v-row>
+  </v-container>
 </template>
+
+<script>
+import { getParsedToken } from "./../../../util/token";
+export default {
+  data: () => ({
+    prescriptions: [],
+  }),
+  mounted() {
+    this.axios
+      .post(
+        process.env.VUE_APP_BACKEND_URL +
+          process.env.VUE_APP_PATIENT_MY_EPRESCRIPTIONS_ENDPOINT,
+        {
+          email: getParsedToken().sub,
+        },
+        {
+          headers: {
+            Authorization: "Bearer " + localStorage.getItem("JWT-CPIS"),
+          },
+        }
+      )
+      .then((resp) => {
+        this.prescriptions = resp.data;
+      })
+      .catch((error) => {
+        alert(error);
+      });
+  },
+};
+</script>
