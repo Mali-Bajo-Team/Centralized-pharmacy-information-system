12f139b58d605506b077e69dfa55fc49a523a469
Pufke <pualic.nemanja@gmail.com>
2021-02-11 23:47:56 +0100

    feat[5.4] transactions for drug perscribe

--- backend-spring-boot/src/main/resources/data-postgres.sql
+++ backend-spring-boot/src/main/resources/data-postgres.sql
@@ -24,6 +24,7 @@ insert into user_authority (user_id, authority_id) values (2, 1);
 insert into person (discriminator, address, city, country, name, surname, phone_number, type) values ('Consultant', 'Jovana Jovanovica 30', 'Novi Sad', 'Srbija', 'Pero', 'Ivanović', '0661352720', 0);
 insert into user_account (email, password, is_active, person_id, needs_password_change) values ('cpisuser+pharmacist@gmail.com', '$2a$10$UYKr0EAl1Hhzmg.3LutLeuczdzcdgEwXhuY0raFaNyRRspKTg0MCW', true, 3, false);
 -- Password for pharmacist account is: pharmacist
+-- Password for pharmacist account is: pharmacist
 insert into user_authority (user_id, authority_id) values (3, 4);
 
 -- INSERT ONE PATIENT
