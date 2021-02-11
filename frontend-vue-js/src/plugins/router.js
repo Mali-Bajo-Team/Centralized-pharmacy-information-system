d86d5856e9ea409052979681e4999b94e26b24b9
milana-todorovic <milanatodorovic@uns.ac.rs>
2021-02-11 19:50:37 +0100

    feat[3.22]: added page for adding promotions

--- frontend-vue-js/src/plugins/router.js
+++ frontend-vue-js/src/plugins/router.js
@@ -44,6 +44,7 @@ import VacationRequests from './../pages/pharmacyadmin/vacation/Vacation';
 import PharmacistSearch from './../pages/pharmacyadmin/consultants/Pharmacists';
 import DermatologistSearch from './../pages/pharmacyadmin/consultants/Dermatologists';
 import DrugOrders from './../pages/pharmacyadmin/orders/Container';
+import Promotion from './../pages/pharmacyadmin/promotions/AddPromotion'
 
 import Supplier from './../pages/supplier/Container';
 import SupplierLanding from './../pages/supplier/landing/Landing';
@@ -294,6 +295,11 @@ const routes = [
                 component: DrugOrders,
                 name: 'pharmacydrugorders',
                 path: 'orders'
+            },
+            {
+                component: Promotion,
+                name: 'addpromotion',
+                path: 'promotion'
             }
         ]
     },
