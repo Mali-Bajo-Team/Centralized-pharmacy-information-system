7d1d34112bad934b0e7874cbdf0cc6a7e9b085a2
Ana Perisic <anaperisictb@yahoo.com>
2021-02-11 14:04:09 +0100

    feat[3.9][frontend]: added templates for history pages

---
+++ frontend-vue-js/src/pages/patient/history/Pharmacist.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-container>
+    <v-row>
+      <!--Left column-->
+      <v-col xl="4" sm="12" md="4">
+        <v-card mt="5">
+          <v-toolbar color="primary" dark dense flat>
+            <v-toolbar-title class="body-2">
+              Sort my consultations
+            </v-toolbar-title>
+          </v-toolbar>
+        </v-card>
+      </v-col>
+      <!--End of left column-->
+
+      <!--Right column-->
+      <v-col xl="8" sm="12" md="8">
+        <v-card>
+            <v-card-subtitle>
+              <h3 class="ml-3">
+                Name of pharmacy,
+                Name of pharmacist
+                Surname of pharmacist ,
+                <!--{{ convertMsToString(consultation.startDate) }}-->
+              </h3>
+            </v-card-subtitle>
+            <v-spacer></v-spacer>
+        </v-card>
+        
+      </v-col>
+      <!--End of right column-->
+    </v-row>
+  </v-container>
+</template>
