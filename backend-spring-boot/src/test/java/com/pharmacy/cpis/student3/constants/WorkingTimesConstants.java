3189f6e7c3ee929661a48178dc919981203458e9
Pufke <pualic.nemanja@gmail.com>
2021-02-12 05:16:06 +0100

    feat-first unit test and initial commit for testing infrastructure

---
+++ backend-spring-boot/src/test/java/com/pharmacy/cpis/student3/constants/WorkingTimesConstants.java
@@ -0,0 +1,19 @@
+package com.pharmacy.cpis.student3.constants;
+
+import java.util.Date;
+
+public class WorkingTimesConstants {
+
+	public static final Date WT_START = new Date(1612940400000L); // 7:00
+
+	public static final Date WT_END = new Date(1612962000000L); // 13:00
+
+	public static final Date WT_OVERLAPPING_START = new Date(1612958400000L); // 12:00
+
+	public static final Date WT_OVERLAPPING_END = new Date(1612983600000L); // 19:00
+
+	public static final Date WT_NOT_OVERLAPPING_START = new Date(1612962000000L); // 13:00
+
+	public static final Date WT_NOT_OVERLAPPING_END = new Date(1612983600000L); // 19:00
+
+}
